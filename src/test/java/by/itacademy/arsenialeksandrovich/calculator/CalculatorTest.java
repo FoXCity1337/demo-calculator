951020005e2916b1a25e70a63218da69b366236c
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-17 19:20:53 +0300

    Создание тестового метода testSum1 для проверки работоспособности базовой операции сложения

--- src/test/java/by/itacademy/arsenialeksandrovich/calculator/CalculatorTest.java
+++ src/test/java/by/itacademy/arsenialeksandrovich/calculator/CalculatorTest.java
@@ -1,4 +1,17 @@
 package by.itacademy.arsenialeksandrovich.calculator;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
+
+    @Test
+    public void testSum1(){
+        int a = 2;
+        int b = 4;
+        Calculator calculator = new Calculator();
+        int actual = calculator.sum(a,b);
+        int expected = 6;
+        Assertions.assertEquals(expected,actual);
+    }
 }
