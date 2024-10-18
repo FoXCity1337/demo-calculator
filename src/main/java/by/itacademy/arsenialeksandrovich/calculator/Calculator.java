23beb06c054f7a3572ec09035e65ada4487aab6c
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-18 22:43:15 +0300

    Убрал лишние пробелы в методе Calculator

--- src/main/java/by/itacademy/arsenialeksandrovich/calculator/Calculator.java
+++ src/main/java/by/itacademy/arsenialeksandrovich/calculator/Calculator.java
@@ -2,19 +2,19 @@ package by.itacademy.arsenialeksandrovich.calculator;
 
 public class Calculator {
 
-    public int sum (int a, int b){
+    public int sum(int a, int b) {
         return a + b;
     }
 
-    public int sub (int a, int b){
+    public int sub(int a, int b) {
         return a - b;
     }
 
-    public int div (int a, int b){
+    public int div(int a, int b) {
         return a / b;
     }
 
-    public int mul (int a, int b){
+    public int mul(int a, int b) {
         return a + b;
     }
 }
